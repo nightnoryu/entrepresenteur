c458d790b0d8f69ca91bc03306bc86ca7a0896c1
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-10-03 15:09:41 +0300

    ISSUE-#2 Перевел 26 функций из TS в JS

--- vars & jsdoc/functions.js
+++ vars & jsdoc/functions.js
@@ -2,3 +2,128 @@
  * @returns Slide
  */
 function createNewSlide() { }
+
+/**
+ * @returns Presentation
+ */
+function createNewPresentation() { }
+
+/**
+ * @returns Editor
+ */
+function createEditor() { }
+
+/**
+ * @returns Presentation
+ */
+function loadPresentation() { }
+
+/**
+ * @returns string
+ */
+function savePresentation() { }
+
+/**
+ * @returns Presentation
+ */
+function setPresentationTitle() { }
+
+/**
+ * @returns Editor
+ */
+function addSlide() { }
+
+/**
+ * @returns Editor
+ */
+function removeSlides() { }
+
+/**
+ * @returns Editor
+ */
+function changeSlidesOrder() { }
+
+/**
+ * @returns Editor
+ */
+function setCurrentSlide() { }
+
+/**
+ * @returns Editor
+ */
+function setSlideBackgroundColor() { }
+
+/**
+ * @returns Editor
+ */
+function setSlideBackgroundImage() { }
+
+/**
+ * @returns Editor
+ */
+function removeElements() { }
+
+/**
+ * @returns Editor
+ */
+function addText() { }
+
+/**
+ * @returns Editor
+ */
+function setTextValue() { }
+
+/**
+ * @returns Editor
+ */
+function setTextFont() { }
+
+/**
+ * @returns Editor
+ */
+function setTextSize() { }
+
+/**
+ * @returns Editor
+ */
+function addImage() { }
+
+/**
+ * @returns Editor
+ */
+function moveElement() { }
+
+/**
+ * @returns Editor
+ */
+function addPrimitive() { }
+
+/**
+ * @returns Editor
+ */
+function setPrimitiveFillColor() { }
+
+/**
+ * @returns Editor
+ */
+function setPrimitiveStrokeColor() { }
+
+/**
+ * @returns Editor
+ */
+function resizeElement() { }
+
+/**
+ * @returns Editor
+ */
+function undo() { }
+
+/**
+ * @returns Editor
+ */
+function redo() { }
+
+/**
+ * @returns Editor
+ */
+function exportPresentation() { }
