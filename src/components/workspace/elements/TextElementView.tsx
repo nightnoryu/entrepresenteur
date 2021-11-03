b2be2dae228f51413533dbdce9a23b6859c804c5
m3tro1d <qazwf734@gmail.com>
2021-11-03 17:07:25 +0300

    ISSUE-#18 Nice looking text element view

--- src/components/workspace/elements/TextElementView.tsx
+++ src/components/workspace/elements/TextElementView.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { TextElement } from '../../../model/types';
+import './ElementView.css';
+import './TextElementView.css';
 
 type TextElementViewProps = {
   textElement: TextElement;
@@ -10,10 +12,20 @@ function TextElementView({ textElement }: TextElementViewProps): JSX.Element {
     color: textElement.color,
     fontFamily: textElement.font,
     fontSize: textElement.size,
+
+    width: textElement.dimensions.width,
+    height: textElement.dimensions.height,
+
+    left: textElement.position.x,
+    top: textElement.position.y,
   };
 
   return (
-    <div className="workspace__element-view" style={textElementStyles}>{textElement.value}</div>
+    <div
+      className="workspace__element-view workspace__element-view_text"
+      style={textElementStyles}
+    >
+      {textElement.value}</div>
   );
 }
 
