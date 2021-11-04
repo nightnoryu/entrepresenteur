c768b361f63f8b1789c27aee05bc0c048cc28dd7
Sergey Patrushev <79759732+NoSiReapo@users.noreply.github.com>
2021-11-04 11:40:53 +0300

    #Sergey Вёрстка визуала компонента slidepanel

--- src/components/slidepanel/SlidePanel.tsx
+++ src/components/slidepanel/SlidePanel.tsx
@@ -5,6 +5,12 @@ function SlidePanel(): JSX.Element {
     return (
         <div className="b-slidepanel">
             SlidePanel
+            <div className="b-slidepanel__slide">
+                Slide block
+                <div className='b-slidepanel__slide-thumbnail'>
+                    thumbnail
+                </div>
+            </div>
         </div>);
 }
 
