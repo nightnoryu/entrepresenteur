10b5cf24a1d5a8841233397e7fda0b89a965c9e9
adelsibga <79917869+adelsibga@users.noreply.github.com>
2021-11-04 15:24:05 +0300

    ISSUE-#20 Сверстал dropdown menu без функций

--- src/components/ribbon/Ribbon.tsx
+++ src/components/ribbon/Ribbon.tsx
@@ -2,12 +2,129 @@ import React from 'react';
 import './Ribbon.css';
 
 function Ribbon(): JSX.Element {
-  return (
-    <div className="ribbon">
-      <h1>New presentation</h1>
-      <p>Menu</p>
-    </div>
-  );
+    return (
+        <div className="ribbon">
+            <h1 className="presentation-name">New presentation</h1>
+            <nav className="menu">
+                <ul className="menu__list">
+                    <li>
+                        <a href="#" className="menu__link">File</a>
+                        <ul className="sub-menu__list">
+                            <li>
+                                <a href="#" className="sub-menu__link">New</a>
+                                <ul className="sub-sub-menu__list">
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Empty</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">From template</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Open</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Rename</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Download</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Export</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="menu__link">Edit</a>
+                        <ul className="sub-menu__list">
+                            <li>
+                                <a href="#" className="sub-menu__link">Undo</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Redo</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Delete</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Text</a>
+                                <ul className="sub-sub-menu__list">
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Color</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Font</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Size</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Primitive</a>
+                                <ul className="sub-sub-menu__list">
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Fill</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Stroke</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="menu__link">Insert</a>
+                        <ul className="sub-menu__list">
+                            <li>
+                                <a href="#" className="sub-menu__link">Image</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Text</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Primitive</a>
+                                <ul className="sub-sub-menu__list">
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Rectangle</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Triangle</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Circle</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="menu__link">Slide</a>
+                        <ul className="sub-menu__list">
+                            <li>
+                                <a href="#" className="sub-menu__link">New</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Delete</a>
+                            </li>
+                            <li>
+                                <a href="#" className="sub-menu__link">Change Background</a>
+                                <ul className="sub-sub-menu__list">
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Color</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className="sub-sub-menu__link">Image</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Ribbon;
