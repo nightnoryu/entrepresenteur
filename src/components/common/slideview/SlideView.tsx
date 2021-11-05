afca0f2283081947e7531c3ad6a7b117ab01788f
m3tro1d <qazwf734@gmail.com>
2021-11-05 18:12:15 +0300

    ISSUE-#18 Remove redundant hook
    Think how to update the scale factor dynamically, without using props

--- src/components/common/slideview/SlideView.tsx
+++ src/components/common/slideview/SlideView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BackgroundType, Dimensions, ElementType, Slide, SlideElement } from '../../../model/types';
 import TextElementView from '../elements/text/TextElementView';
 import ImageElementView from '../elements/image/ImageElementView';
@@ -39,7 +39,7 @@ function scaleElement(element: SlideElement, scaleFactor?: number): SlideElement
 
 function SlideView({ slide, scaleFactor }: SlideViewProps): JSX.Element {
   const scaledDimensions = getScaledSlideDimensions(slide, scaleFactor);
-  let slideStyle = slide.background.type == BackgroundType.SOLID
+  const slideStyle = slide.background.type == BackgroundType.SOLID
     ? {
       backgroundColor: slide.background.color,
       ...scaledDimensions,
@@ -48,14 +48,6 @@ function SlideView({ slide, scaleFactor }: SlideViewProps): JSX.Element {
       background: `url(${slide.background.src})`,
       ...scaledDimensions,
     };
-
-  useEffect(() => {
-    const scaledDimensions = getScaledSlideDimensions(slide, scaleFactor);
-    slideStyle = {
-      ...slideStyle,
-      ...scaledDimensions,
-    };
-  }, [scaleFactor]);
 
   return (
     <div className="slideview" style={slideStyle}>
