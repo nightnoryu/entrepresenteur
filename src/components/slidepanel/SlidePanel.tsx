edae2c7be006715cdc977128baaff8b136a2fd04
m3tro1d <qazwf734@gmail.com>
2021-11-14 13:37:47 +0300

    Merge formatting

--- src/components/slidepanel/SlidePanel.tsx
+++ src/components/slidepanel/SlidePanel.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './SlidePanel.css';
 
 function SlidePanel(): JSX.Element {
-    return (
-        <div className="slidepanel">
-            SlidePanel
-            <div className="slidepanel__slide">
-                Slide block
-                <div className='slidepanel__slide-thumbnail'>
-                    thumbnail
-                </div>
-            </div>
-        </div>);
+  return (
+    <div className="slidepanel">
+      SlidePanel
+      <div className="slidepanel__slide">
+        Slide block
+        <div className="slidepanel__slide-thumbnail">
+          thumbnail
+        </div>
+      </div>
+    </div>);
 }
 
 export default SlidePanel;
