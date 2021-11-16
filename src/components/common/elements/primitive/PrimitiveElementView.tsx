93f7efc1cf3b69be38d84b2060ca347f2f77367a
m3tro1d <qazwf734@gmail.com>
2021-11-16 21:20:08 +0300

    ISSUE-#27 Implement basic drag and drop

--- src/components/common/elements/primitive/PrimitiveElementView.tsx
+++ src/components/common/elements/primitive/PrimitiveElementView.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PrimitiveElement } from '../../../../model/types';
 import '../ElementView.css';
 import { selectPrimitive } from '../../../../common/componentsFunctions';
+import useDragAndDrop from '../../../../hooks/useDragAndDrop';
 
 type PrimitiveElementViewProps = {
   element: PrimitiveElement;
 }
 
 function PrimitiveElementView({ element }: PrimitiveElementViewProps): JSX.Element {
+  const ref = useRef<HTMLDivElement>(null);
+  const position = useDragAndDrop(ref, element.position);
+
   return (
     <div
       className="workspace__element-view"
       style={{
         width: element.dimensions.width,
         height: element.dimensions.height,
-        left: element.position.x,
-        top: element.position.y,
+        left: position.x,
+        top: position.y,
       }}
+      draggable={false}
+      ref={ref}
     >
       {selectPrimitive(element)}
     </div>
--- src/components/common/elements/text/TextElementView.tsx
+++ src/components/common/elements/text/TextElementView.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { TextElement } from '../../../../model/types';
 import '../ElementView.css';
 import './TextElementView.css';
+import useDragAndDrop from '../../../../hooks/useDragAndDrop';
 
 type TextElementViewProps = {
   element: TextElement;
 }
 
 function TextElementView({ element }: TextElementViewProps): JSX.Element {
+  const ref = useRef<HTMLDivElement>(null);
+  const position = useDragAndDrop(ref, element.position);
+
   return (
     <div
       className="workspace__element-view workspace__element-view_text"
@@ -17,9 +21,11 @@ function TextElementView({ element }: TextElementViewProps): JSX.Element {
         fontSize: element.size,
         width: element.dimensions.width,
         height: element.dimensions.height,
-        left: element.position.x,
-        top: element.position.y,
+        left: position.x,
+        top: position.y,
       }}
+      draggable={false}
+      ref={ref}
     >
       {element.value}</div>
   );
