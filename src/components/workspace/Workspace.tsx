ab1e2666c505d923aa2580cd0a9b838760cea3e5
m3tro1d <qazwf734@gmail.com>
2021-11-21 20:44:16 +0300

    ISSUE-#27 Workspace on CSS modules

--- src/components/workspace/Workspace.tsx
+++ src/components/workspace/Workspace.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Workspace.css';
+import styles from './Workspace.module.css';
 import { Slide } from '../../model/types';
 import SlideView from '../common/slideview/SlideView';
 import { UUID } from '../../model/uuid';
@@ -13,8 +13,8 @@ function Workspace({ slide, selectedElementIDs }: WorkspaceProps): JSX.Element {
   const displayedSlide = slide || createNewSlide();
 
   return (
-    <div className="workspace">
-      <div className="workspace__slideview-wrapper">
+    <div className={styles.workspace}>
+      <div className={styles.wrapper}>
         <SlideView slide={displayedSlide} />
       </div>
     </div>
