ad3a810735985540179ef5fa79952469fd1fb0bc
m3tro1d <qazwf734@gmail.com>
2021-11-27 21:48:48 +0300

    ISSUE-#36 Implement presentation opening and saving
    Wacky first take

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,62 @@ import Workspace from './components/workspace/Workspace';
 import './App.css';
 import useConfirmLeaving from './hooks/useConfirmLeaving';
 import { isCurrentSlide } from './model/model_utils';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './state/reducers';
+import useEventListener from './hooks/useEventListener';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state';
 
 function App(): JSX.Element {
   const editor = useSelector((state: RootState) => state.editor);
+  const dispatch = useDispatch();
+  const { openPresentation } = bindActionCreators(actionCreators, dispatch);
 
   const currentSlide = editor.presentation.slides.find(slide => isCurrentSlide(slide, editor.selectedSlideIDs));
 
   useConfirmLeaving();
+  useEventListener('keydown', (e: Event) => {
+    const event = e as KeyboardEvent;
+    if (event.ctrlKey && event.key === 's') {
+      e.preventDefault();
+
+      const file = new Blob([JSON.stringify(editor.presentation)], { type: 'text/plain' });
+      const a = document.createElement('a');
+      const url = URL.createObjectURL(file);
+
+      a.href = url;
+      a.download = `${editor.presentation.title}.entrepresenteur.json`;
+      a.click();
+      setTimeout(() => {
+        URL.revokeObjectURL(url);
+      }, 0);
+
+    } else if (event.ctrlKey && event.key === 'o') {
+      e.preventDefault();
+
+      const input = document.createElement('input');
+      input.type = 'file';
+
+      input.onchange = e => {
+        const target = e.target as HTMLInputElement;
+        if (target?.files) {
+          const file = target.files[0];
+
+          const reader = new FileReader();
+          reader.readAsText(file, 'UTF-8');
+
+          reader.onload = readerEvent => {
+            if (readerEvent.target?.result) {
+              const content = readerEvent.target.result.toString() || '';
+              openPresentation(JSON.parse(content));
+            }
+          };
+        }
+      };
+
+      input.click();
+    }
+  });
 
   return (
     <div className="app">
