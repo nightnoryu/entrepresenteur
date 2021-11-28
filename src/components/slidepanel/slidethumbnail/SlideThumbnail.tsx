7a2be4f2614a783fbf209b26e719402bc562d61f
m3tro1d <qazwf734@gmail.com>
2021-11-28 18:06:04 +0300

    ISSUE-#36 Simplify slide thumbnail clicking

--- src/components/slidepanel/slidethumbnail/SlideThumbnail.tsx
+++ src/components/slidepanel/slidethumbnail/SlideThumbnail.tsx
@@ -1,10 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import '../../slideview/SlideView';
 import { Slide } from '../../../model/types';
 import styles from './SlideThumbnail.module.css';
 import SlideView from '../../slideview/SlideView';
 import { createNewSlide } from '../../../model/model_utils';
-import useEventListener from '../../../hooks/useEventListener';
 
 type SlideThumbnailProps = {
   slide: Slide;
@@ -16,15 +15,8 @@ type SlideThumbnailProps = {
 function SlideThumbnail({ slide, index, isSelected, onClick }: SlideThumbnailProps): JSX.Element {
   const displayedSlide = slide || createNewSlide();
 
-  const ref = useRef(null);
-  useEventListener('click', () => {
-    if (onClick) {
-      onClick();
-    }
-  }, ref);
-
   return (
-    <div className={styles.slidethumbnail} ref={ref}>
+    <div className={styles.slidethumbnail} onClick={onClick}>
       <span className={styles.slideindex}>{index}</span>
       <div className={styles.slideview}>
         <SlideView slide={displayedSlide} />
