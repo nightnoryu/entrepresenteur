b14a6b88c84dfd09e1f79fde3a81a5d75fc6a8b0
m3tro1d <qazwf734@gmail.com>
2021-12-05 19:01:00 +0300

    ISSUE-#43 Refactor hotkeys binding

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,11 @@ import useConfirmLeaving from './hooks/useConfirmLeaving';
 import { connect, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from './state';
-import { openImageBase64, openPresentationJSON, savePresentationJSON } from './common/fileUtils';
-import useHotkeyCtrl from './hooks/hotkeys/useHotkeyCtrl';
 import { Presentation } from './model/types';
 import { RootState } from './state/reducers';
 import { menuItems } from './model/menu';
 import Workspace from './components/Workspace/Workspace';
+import useAppHotkeys from './hooks/hotkeys/useAppHotkeys';
 
 type AppProps = {
   presentation: Presentation;
@@ -23,7 +22,6 @@ function App({ presentation }: AppProps): JSX.Element {
     openPresentation,
     newPresentation,
     addSlide,
-    removeSlides,
     setSlideBackgroundImage,
     addText,
     addImage,
@@ -32,48 +30,17 @@ function App({ presentation }: AppProps): JSX.Element {
   } = bindActionCreators(actionCreators, dispatch);
 
   useConfirmLeaving();
-  useHotkeyCtrl('s', () => {
-    savePresentationJSON(presentation, presentation.title);
-  });
-  useHotkeyCtrl('o', () => {
-    openPresentationJSON()
-      .then(presentation => openPresentation(presentation))
-      .catch(error => alert(error));
-  });
-  useHotkeyCtrl('m', () => {
-    const confirmed = confirm('Are you sure?');
-    if (confirmed) {
-      newPresentation();
-    }
-  });
-  useHotkeyCtrl('l', () => {
-    addSlide();
-  });
-  useHotkeyCtrl('d', () => {
-    removeSlides();
-  });
-  useHotkeyCtrl('b', () => {
-    openImageBase64()
-      .then(image => setSlideBackgroundImage(image.src))
-      .catch(error => alert(error));
-  });
-  useHotkeyCtrl('e', () => {
-    const text = prompt('Enter text') || '';
-    if (text !== '') {
-      addText({ x: 0, y: 0 }, { width: 0, height: 0 }, text);
-    }
-  });
-  useHotkeyCtrl('i', () => {
-    openImageBase64()
-      .then(image => addImage({ x: 0, y: 0 }, { width: image.width, height: image.height }, image.src))
-      .catch(error => alert(error));
-  });
-  useHotkeyCtrl('z', () => {
-    undo();
-  });
-  useHotkeyCtrl('y', () => {
-    redo();
-  });
+  useAppHotkeys(
+    presentation,
+    addImage,
+    addSlide,
+    addText,
+    newPresentation,
+    openPresentation,
+    redo,
+    setSlideBackgroundImage,
+    undo,
+  );
 
   return (
     <div className="app">
---
+++ src/hooks/hotkeys/useAppHotkeys.ts
@@ -0,0 +1,76 @@
+import useHotkeyCtrl from './useHotkeyCtrl';
+import { openImageBase64, openPresentationJSON, savePresentationJSON } from '../../common/fileUtils';
+import { Presentation } from '../../model/types';
+import {
+  addImage as addImageCreator,
+  addSlide as addSlideCreator,
+  addText as addTextCreator,
+  newPresentation as newPresentationCreator,
+  openPresentation as openPresentationCreator,
+  redo as redoCreator,
+  setSlideBackgroundImage as setSlideBackgroundImageCreator,
+  undo as undoCreator,
+} from '../../state/actions/actionCreators';
+
+function useAppHotkeys(
+  presentation: Presentation,
+  addImage: typeof addImageCreator,
+  addSlide: typeof addSlideCreator,
+  addText: typeof addTextCreator,
+  newPresentation: typeof newPresentationCreator,
+  openPresentation: typeof openPresentationCreator,
+  redo: typeof redoCreator,
+  setSlideBackgroundImage: typeof setSlideBackgroundImageCreator,
+  undo: typeof undoCreator,
+): void {
+  useHotkeyCtrl('s', () => {
+    savePresentationJSON(presentation, presentation.title);
+    console.log('whatever');
+  });
+
+  useHotkeyCtrl('o', () => {
+    openPresentationJSON()
+      .then(presentation => openPresentation(presentation))
+      .catch(error => alert(error));
+  });
+
+  useHotkeyCtrl('m', () => {
+    const confirmed = confirm('Are you sure?');
+    if (confirmed) {
+      newPresentation();
+    }
+  });
+
+  useHotkeyCtrl('l', () => {
+    addSlide();
+  });
+
+  useHotkeyCtrl('b', () => {
+    openImageBase64()
+      .then(image => setSlideBackgroundImage(image.src))
+      .catch(error => alert(error));
+  });
+
+  useHotkeyCtrl('e', () => {
+    const text = prompt('Enter text') || '';
+    if (text !== '') {
+      addText({ x: 0, y: 0 }, { width: 0, height: 0 }, text);
+    }
+  });
+
+  useHotkeyCtrl('i', () => {
+    openImageBase64()
+      .then(image => addImage({ x: 0, y: 0 }, { width: image.width, height: image.height }, image.src))
+      .catch(error => alert(error));
+  });
+
+  useHotkeyCtrl('z', () => {
+    undo();
+  });
+
+  useHotkeyCtrl('y', () => {
+    redo();
+  });
+}
+
+export default useAppHotkeys;
