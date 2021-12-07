88c600488476c67e8c353002c694153c4bbad252
m3tro1d <qazwf734@gmail.com>
2021-12-07 21:14:51 +0300

    ISSUE-#29 Add some styling

--- src/components/Ribbon/DropdownMenu/DropdownMenu.tsx
+++ src/components/Ribbon/DropdownMenu/DropdownMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DropdownMenuItem } from '../RibbonTypes';
+import styles from './DropdownMenu.module.css';
 
 type DropdownMenuProps = {
   menuItems: DropdownMenuItem[];
@@ -7,10 +8,12 @@ type DropdownMenuProps = {
 
 function DropdownMenu({ menuItems }: DropdownMenuProps): JSX.Element {
   return (
-    <div>
-      {menuItems.map(menuItem => {
-        return <div key={menuItem.label}>{menuItem.label}</div>;
-      })}
+    <div className={styles.menu}>
+      {menuItems.map(menuItem => (
+        <div key={menuItem.label} className={styles.item}>
+          <div className={styles.itemLabel}>{menuItem.label}</div>
+        </div>
+      ))}
     </div>
   );
 }
