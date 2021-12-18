85670f4b7b853fa63ced90b04c7dc4d01579ba83
m3tro1d <qazwf734@gmail.com>
2021-12-18 10:49:55 +0300

    ISSUE-#51 Implement changing font

--- src/components/Ribbon/PropertiesPanel/TextPropertiesPanel/TextPropertiesPanel.tsx
+++ src/components/Ribbon/PropertiesPanel/TextPropertiesPanel/TextPropertiesPanel.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
 import styles from './TextPropertiesPanel.module.css';
 import { getTextProperties } from '../../../../model/uiParameters/textProperties';
+import { connect, useDispatch } from 'react-redux';
+import { actionCreators } from '../../../../state';
+import { bindActionCreators } from 'redux';
+import { UUID } from '../../../../model/uuid';
+import { RootState } from '../../../../state/reducers';
 
-function TextPropertiesPanel(): JSX.Element {
+type TextPropertiesPanelProps = {
+  currentElementID: UUID;
+};
+
+function TextPropertiesPanel({ currentElementID }: TextPropertiesPanelProps): JSX.Element {
+  const dispatch = useDispatch();
+  const { setTextFont, setTextSize } = bindActionCreators(actionCreators, dispatch);
+
   const textProps = getTextProperties();
+
+  const onFontChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setTextFont(currentElementID, event.currentTarget.value);
+  };
 
   return (
     <ul className={styles.textPropertiesPanel}>
       <li className={styles.panelElement}>
-        <select name="font">
+        <select
+          name="font"
+          className={styles.fontSelection}
+          onChange={onFontChange}
+        >
           {textProps.fonts.map(fontOption => (
-            <option key={fontOption} value={fontOption}>{fontOption}</option>
+            <option
+              key={fontOption}
+              value={fontOption}
+            >
+              {fontOption}
+            </option>
           ))}
         </select>
       </li>
-      <li className={styles.panelElement}>Color</li>
+      <li className={styles.panelElement}>Size</li>
     </ul>
   );
 }
 
-export default TextPropertiesPanel;
+function mapStateToProps(state: RootState): TextPropertiesPanelProps {
+  return {
+    currentElementID: state.selectedElementIDs[0],
+  };
+}
+
+export default connect(mapStateToProps)(TextPropertiesPanel);
