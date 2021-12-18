f264b1ba6517893aaca0c7c345b049f64c8aedbb
m3tro1d <qazwf734@gmail.com>
2021-12-18 11:16:47 +0300

    ISSUE-#51 Add primitive properties panel template

--- src/components/Ribbon/PropertiesPanel/PrimitivePropertiesPanel/PrimitivePropertiesPanel.tsx
+++ src/components/Ribbon/PropertiesPanel/PrimitivePropertiesPanel/PrimitivePropertiesPanel.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from './PrimitivePropertiesPanel.module.css';
 
 function PrimitivePropertiesPanel(): JSX.Element {
   return (
-    <li>Primitive</li>
+    <ul className={styles.primitivePropertiesPanel}>
+      <li className={styles.panelElement}>
+        Fill
+      </li>
+
+      <li className={styles.panelElement}>
+        Stroke
+      </li>
+    </ul>
   );
 }
 
