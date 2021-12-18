80038a0e9abc65213639b94bd72e115fc9854aea
m3tro1d <qazwf734@gmail.com>
2021-12-18 17:23:03 +0300

    ISSUE-#51 Implement primitive properties panel

--- src/components/Ribbon/PropertiesPanel/PrimitivePropertiesPanel/PrimitivePropertiesPanel.tsx
+++ src/components/Ribbon/PropertiesPanel/PrimitivePropertiesPanel/PrimitivePropertiesPanel.tsx
@@ -1,18 +1,71 @@
 import React from 'react';
 import styles from './PrimitivePropertiesPanel.module.css';
+import { connect, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../../../../state';
+import { ElementType, PrimitiveElement } from '../../../../model/types';
+import { RootState } from '../../../../state/reducers';
+import { isCurrentElement, isCurrentSlide } from '../../../../model/modelUtils';
 
-function PrimitivePropertiesPanel(): JSX.Element {
+type PrimitivePropertiesPanelProps = {
+  currentElement?: PrimitiveElement;
+};
+
+function PrimitivePropertiesPanel({ currentElement }: PrimitivePropertiesPanelProps): JSX.Element {
+  const dispatch = useDispatch();
+  const { setPrimitiveFillColor, setPrimitiveStrokeColor } = bindActionCreators(actionCreators, dispatch);
+
+  const onFillColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (currentElement) {
+      setPrimitiveFillColor(currentElement.id, event.currentTarget.value);
+    }
+  };
+
+  const onStrokeColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (currentElement) {
+      setPrimitiveStrokeColor(currentElement.id, event.currentTarget.value);
+    }
+  };
+
   return (
     <ul className={styles.primitivePropertiesPanel}>
       <li className={styles.panelElement}>
         Fill
+        <input
+          type="color"
+          defaultValue={currentElement?.fill}
+          onInput={onFillColorChange}
+        />
       </li>
 
       <li className={styles.panelElement}>
         Stroke
+        <input
+          type="color"
+          defaultValue={currentElement?.stroke}
+          onInput={onStrokeColorChange}
+        />
       </li>
     </ul>
   );
 }
 
-export default PrimitivePropertiesPanel;
+function mapStateToProps(state: RootState): PrimitivePropertiesPanelProps {
+  for (const slide of state.presentation.slides) {
+    if (isCurrentSlide(slide, state.selectedSlideIDs)) {
+      for (const element of slide.elements) {
+        if (isCurrentElement(element, state.selectedElementIDs) && element.type === ElementType.PRIMITIVE) {
+          return {
+            currentElement: element,
+          };
+        }
+      }
+    }
+  }
+
+  return {
+    currentElement: undefined,
+  };
+}
+
+export default connect(mapStateToProps)(PrimitivePropertiesPanel);
