206937c851057ba1a136ae3581d7744797a7b325
m3tro1d <qazwf734@gmail.com>
2021-12-22 21:25:59 +0300

    Remove preloaded state

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
-import getEditor from './editor';
 
 const composeEnhancers = (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
 
 const store = createStore(
   reducer,
-  getEditor(),
+  undefined,
   composeEnhancers(applyMiddleware(thunk)),
 );
 
