88997fca72bf676c5a9c5efd6a436cfe45473c12
m3tro1d <qazwf734@gmail.com>
2022-01-06 10:10:05 +0300

    ISSUE-#52 Implement slide pages, text and document title

--- src/common/pdfExporter.ts
+++ src/common/pdfExporter.ts
@@ -1,7 +1,44 @@
-import { Presentation } from '../model/types';
+import { ElementType, Presentation, Slide } from '../model/types';
+import jsPDF from 'jspdf';
+import { SLIDE_HEIGHT, SLIDE_WIDTH } from '../model/constants';
+
+function fillSlidePage(pdf: jsPDF, slide: Slide): void {
+  slide.elements.map(element => {
+    switch (element.type) {
+    case ElementType.TEXT:
+      pdf
+        .setFont(element.font)
+        .setFontSize(element.size)
+        .text(element.value, element.position.x, element.position.y);
+      break;
+    default:
+      break;
+    }
+  });
+}
 
 function exportPresentationPDF(presentation: Presentation, filename: string): void {
-  console.log('Export');
+  if (presentation.slides.length === 0) {
+    return;
+  }
+
+  const pdf = new jsPDF({
+    orientation: 'landscape',
+    format: [SLIDE_WIDTH, SLIDE_HEIGHT],
+    unit: 'px',
+  });
+  pdf.setDocumentProperties({
+    title: presentation.title,
+  });
+
+  fillSlidePage(pdf, presentation.slides[0]);
+
+  presentation.slides.slice(1).map(slide => {
+    pdf.addPage();
+    fillSlidePage(pdf, slide);
+  });
+
+  pdf.save(filename);
 }
 
 export default exportPresentationPDF;
