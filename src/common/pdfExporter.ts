24544ed5ac12a4ce5c960a8bd7f8182439d26b7f
m3tro1d <qazwf734@gmail.com>
2022-01-06 09:48:20 +0300

    ISSUE-#52 Add PDF generation template code

---
+++ src/common/pdfExporter.ts
@@ -0,0 +1,7 @@
+import { Presentation } from '../model/types';
+
+function exportPresentationPDF(presentation: Presentation, filename: string): void {
+  console.log('Export');
+}
+
+export default exportPresentationPDF;
