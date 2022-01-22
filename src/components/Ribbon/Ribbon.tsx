98820e8a2d6850b8fc9cbfcef1a32dbc8417c2c9
m3tro1d <qazwf734@gmail.com>
2022-01-22 16:10:41 +0300

    ISSUE-#50 Add animated logo

--- src/components/Ribbon/Ribbon.tsx
+++ src/components/Ribbon/Ribbon.tsx
@@ -4,6 +4,7 @@ import PresentationTitle from './PresentationTitle/PresentationTitle';
 import DropdownMenu from './DropdownMenu/DropdownMenu';
 import { DropdownMenuItemType } from './RibbonTypes';
 import PropertiesPanel from './PropertiesPanel/PropertiesPanel';
+import logo from './logo.svg';
 
 type RibbonProps = {
   menuItems: DropdownMenuItemType[];
@@ -12,6 +13,7 @@ type RibbonProps = {
 function Ribbon({ menuItems }: RibbonProps): JSX.Element {
   return (
     <div className={styles.ribbon}>
+      <img src={logo} alt="Logo" className={styles.logo} />
       <PresentationTitle />
       <DropdownMenu menuItems={menuItems} />
       <PropertiesPanel />
